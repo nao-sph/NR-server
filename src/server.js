66d5935684edd9fc968278d4f7f6a3d30b648e54
nao-sph <naoki.nagai@icloud.com>
2017-08-19 18:59:55 +0900

    bugfix

--- src/server.js
+++ src/server.js
@@ -72,16 +72,16 @@ io.on('connection', (socket) => {
   //   }
   // })
   socket.on('req_to_room_without_self', (data) => { // (roomが使われてる時のみ) 自分の所属するroomの全員
-    console.log('to_room_without_self', data))
+    console.log('to_room_without_self', data)
     let roomInfo = RM.getRoomInfo(socket.id)
     if(roomInfo === -1) {
       err = new Error("you haven't joined any room yet")
-      io.to(socke.id).emit('to_room_without_self', data))
+      io.to(socke.id).emit('to_room_without_self', data)
     }
     for(let i = 0; i < roomInfo.members.length; i++) {
       let uid = roomInfo.members[i].id
       if(uid === socket.id) continue
-      io.to().emit('to_room_without_self', data))
+      io.to().emit('to_room_without_self', data)
     }
   })
 
