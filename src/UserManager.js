a88bbb7c5a00c047832cebde619dfc908b84e7da
nao-sph <naoki.nagai@icloud.com>
2017-08-19 12:47:47 +0900

    add UserManager.js

---
+++ src/UserManager.js
@@ -0,0 +1,31 @@
+class UserManager {
+  constructor () {
+    this.users = []
+  }
+
+  add (id, data) {
+    this.users.push(new User(id, data))
+  }
+  delete (id) {
+    this.users.splice(this.getIdx(id), 1)
+  }
+  getData (id) {
+    this.users[getIdx(id)].data
+  }
+
+  getIdx (id) {
+    for (let i = 0; i < this.users.length; i++) {
+      if(this.users[i].ID === id) {
+        return i
+      }
+    }
+    return -1
+  }
+}
+
+class User (id, data) {
+  constructor () {
+    this.ID = id
+    this.data = data
+  }
+}
